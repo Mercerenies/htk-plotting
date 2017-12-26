abc1d2f7aeadbb9831ae983c636b78a2fda3d64e
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-12-25 21:02:18 -0600

    Cosmetic touchups

--- Main.hs
+++ Main.hs
@@ -15,17 +15,19 @@ clickEvent entry cnvs = do
 
 main :: IO ()
 main = do
-  mainwin <- initHTk [minSize (cm 15, cm 15), maxSize (cm 15, cm 15)]
+  mainwin <- initHTk [minSize (cm 15, cm 15), maxSize (cm 15, cm 15), text "Plotting"]
   cnvs <- newCanvas mainwin [background "white", size (cm 15, cm 15)]
   frame <- newFrame mainwin []
-  entry <- newEntry frame []
+  entry <- newEntry frame [value "x * (x - 1) * (x + 1)"]
   btn <- newButton frame [text "Plot"]
   click <- clicked btn
+  let event = clickEvent entry cnvs
   (ret, _) <- bindSimple entry (KeyRelease $ Just (KeySym "Return"))
-  _ <- spawnEvent $ forever (click >>> clickEvent entry cnvs +> ret >>> clickEvent entry cnvs)
+  _ <- spawnEvent $ forever (click >>> event +> ret >>> event)
   pack frame [Fill X, Side AtBottom]
   pack entry [Fill X, Side AtLeft, Expand On]
   pack btn [Fill None, Side AtRight]
   pack cnvs [Fill Both, Side AtTop, Expand On]
   forceFocus entry
+  event
   finishHTk
